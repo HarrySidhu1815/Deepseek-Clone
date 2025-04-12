78193f33eae4ddcdd07e55f9166502fd0c4b2ff6
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2025-04-11 19:40:24 -0700

    Message component added

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 'use client'
 import { assets } from "@/assets/assets";
+import Message from "@/components/Message";
 import PromptBox from "@/components/PromptBox";
 import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
@@ -33,7 +34,7 @@ export default function Home() {
           ) :
           (
           <div>
-
+            <Message role={'user'} content={'What is Deepseek?'}/>
           </div>
         )}
 
